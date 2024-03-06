126311c5f10eb4ce09e159aa1fe4f4792fa5e657
MURTAZA_AFRIDI_99 <160273309+MURTAZA-AFRIDI-01@users.noreply.github.com>
2024-03-06 02:38:46 -0800

    Delete 2 personal message.ts

--- 2 personal message.ts
+++
@@ -1,9 +0,0 @@
-
-let personname :string= '';
-personname = prompt('what is your name?')||'';
-if(personname !==null && personname !==''){
-    alert(`Hello ${personname}, would you like to learn some Python today?`)
-}
-else{
-alert('you have ton fill your name!')
-}
