86178305b40b74d593bd0ec9038dea3d7c87563c
MURTAZA_AFRIDI_99 <160273309+MURTAZA-AFRIDI-01@users.noreply.github.com>
2024-03-06 03:26:41 -0800

    Add files via upload

---
+++ 14.Guest List/14.Guest_List.js
@@ -0,0 +1,5 @@
+"use strict";
+const GUEST_LIST = ['mohsan', 'sami', 'bilawal'];
+for (let i = 0; i < GUEST_LIST.length; i++) {
+    console.log('Dear Mr. ' + GUEST_LIST[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nTHANKS YOU!');
+}
