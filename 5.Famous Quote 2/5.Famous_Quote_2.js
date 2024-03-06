194e64eb4ec9a5ac021e50c0b884d032024753ae
MURTAZA_AFRIDI_99 <160273309+MURTAZA-AFRIDI-01@users.noreply.github.com>
2024-03-06 03:04:12 -0800

    Add files via upload

---
+++ 5.Famous Quote 2/5.Famous_Quote_2.js
@@ -0,0 +1,4 @@
+"use strict";
+let Famous_Person = 'Steven Bartlett';
+let message = '“The Magic You Are Looking For Is In The Work You Are Avoiding .”';
+console.log(`${Famous_Person} once said, ${message}`);
---
+++ 5.Famous Quote 2/5.Famous_Quote_2.ts
@@ -0,0 +1,3 @@
+let Famous_Person :string = 'Steven Bartlett';
+let message :string = '“The Magic You Are Looking For Is In The Work You Are Avoiding .”'
+console.log(`${Famous_Person} once said, ${message}`);
