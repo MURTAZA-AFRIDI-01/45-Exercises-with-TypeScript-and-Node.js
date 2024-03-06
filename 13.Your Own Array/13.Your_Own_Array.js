dda36538ba06fd5856387855938edb95c557a8fc
MURTAZA_AFRIDI_99 <160273309+MURTAZA-AFRIDI-01@users.noreply.github.com>
2024-03-06 03:25:14 -0800

    Add files via upload

---
+++ 13.Your Own Array/13.Your_Own_Array.js
@@ -0,0 +1,5 @@
+"use strict";
+const transprotation = ['motorcycle', 'car', 'train', 'cycle', 'NINJA H2R'];
+for (let i = 0; i < transprotation.length; i++) {
+    console.log('I would like to own a ' + transprotation[i]);
+}
