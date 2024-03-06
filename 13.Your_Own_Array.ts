538a2da196c0e3c1844f777d23980201dac52a0c
MURTAZA_AFRIDI_99 <160273309+MURTAZA-AFRIDI-01@users.noreply.github.com>
2024-03-06 02:36:55 -0800

    Delete 13.Your_Own_Array.ts

--- 13.Your_Own_Array.ts
+++
@@ -1,4 +0,0 @@
-const transprotation :string[] = [ 'motorcycle', 'car', 'train', 'cycle' , 'NINJA H2R'];
-for( let i=0;i<transprotation.length; i++ ){
-    console.log('I would like to own a ' + transprotation[i])
-}
