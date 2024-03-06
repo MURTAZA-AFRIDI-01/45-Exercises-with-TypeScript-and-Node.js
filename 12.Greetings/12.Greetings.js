761f07b71b7f9ed51cc09921303fbc092cd953aa
MURTAZA_AFRIDI_99 <160273309+MURTAZA-AFRIDI-01@users.noreply.github.com>
2024-03-06 03:23:03 -0800

    Add files via upload

---
+++ 12.Greetings/12.Greetings.js
@@ -0,0 +1,6 @@
+"use strict";
+const names = ['komi', 'abdullah', 'asfand', 'khizar' , 'hamza tayyab' , 'rana tayyab'];
+const message = 'wellcome to class buddy: ';
+for (let i = 0; i < names.length; i++) {
+    console.log(message + names[i]);
+}
