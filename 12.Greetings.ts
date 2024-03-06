ca681ae3227a16d4f053dc93c80c721c52db494c
MURTAZA_AFRIDI_99 <160273309+MURTAZA-AFRIDI-01@users.noreply.github.com>
2024-03-06 02:36:43 -0800

    Delete 12.Greetings.ts

--- 12.Greetings.ts
+++
@@ -1,7 +0,0 @@
- const names :string[] =['komi','abdullah','asfand','khizar']
- const message :string ='wellcome to class buddy: '
- for(let i=0; i<names.length; i++){
-
-    console.log(message + names[i]) ;
-
-}
