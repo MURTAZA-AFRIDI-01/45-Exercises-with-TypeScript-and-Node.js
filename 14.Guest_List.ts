c767a949a8b58460d3ed64d1818ce345d8ee83ac
MURTAZA_AFRIDI_99 <160273309+MURTAZA-AFRIDI-01@users.noreply.github.com>
2024-03-06 02:37:14 -0800

    Delete 14.Guest_List.ts

--- 14.Guest_List.ts
+++
@@ -1,4 +0,0 @@
- const GUEST_LIST :string[] =['mohsan', 'sami', 'bilawal'];
- for( let i=0;i<GUEST_LIST.length; i++){
-    console.log('Dear Mr. ' + GUEST_LIST[i] + ',\n\nIt is our pleasure to invite you in our party.\n\nTHANKS YOU!')
- }
