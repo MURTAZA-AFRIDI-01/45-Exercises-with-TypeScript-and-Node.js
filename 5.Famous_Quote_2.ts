cb540764688cbdc562031cca89e13703041234be
MURTAZA_AFRIDI_99 <160273309+MURTAZA-AFRIDI-01@users.noreply.github.com>
2024-03-06 02:39:26 -0800

    Delete 5.Famous_Quote_2.ts

--- 5.Famous_Quote_2.ts
+++
@@ -1,3 +0,0 @@
-let Famous_Person :string = 'Steven Bartlett';
-let message :string = '“The Magic You Are Looking For Is In The Work You Are Avoiding .”'
-console.log(`${Famous_Person} once said, ${message}`);
