fc3a5642df1272b536c4dd8945b5499331c788c6
MURTAZA_AFRIDI_99 <160273309+MURTAZA-AFRIDI-01@users.noreply.github.com>
2024-03-09 23:57:21 +0500

    Add files via upload

---
+++ 20.Created Array/Created_Array.js
@@ -0,0 +1,9 @@
+"use strict";
+// Think of something you could store in a array.
+let created_array = ['Programming', 'Analytics', 'Artificial_intelligence'];
+//  Write a program that creates a list containing these items.
+let created_array2 = [];
+created_array2.push('germany');
+created_array2.push('england');
+created_array2.push('india');
+console.log(created_array2);
