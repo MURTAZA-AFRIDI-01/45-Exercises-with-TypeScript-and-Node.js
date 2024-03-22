a8ac1e78c99b821ca13ac9bd20f4ee67651b069d
MURTAZA_AFRIDI_99 <160273309+MURTAZA-AFRIDI-01@users.noreply.github.com>
2024-03-23 03:52:23 +0500

    Add files via upload

---
+++ 41.Magicians/magicians.js
@@ -0,0 +1,9 @@
+"use strict";
+// Make a array of magician’s names. Pass the array to a function called show_magicians(), which prints the name of each magician in the array.
+let magician = ['murtaza', 'junaid', 'affan', 'numan'];
+function show_magicians(magicians) {
+    magicians.forEach(element => {
+        console.log(element);
+    });
+}
+show_magicians(magician);
