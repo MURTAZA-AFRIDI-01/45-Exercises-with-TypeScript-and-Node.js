02fb6da578d69d5b42293d5140cce6e309786456
MURTAZA_AFRIDI_99 <160273309+MURTAZA-AFRIDI-01@users.noreply.github.com>
2024-03-23 03:55:35 +0500

    Add files via upload

---
+++ 45.Cars/cars.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+// Write a function that stores information about a car in a Object. The function should always receive a manufacturer and a model name. It should then accept an arbitrary number of keyword arguments. Call the function with the required information and two other name-value pairs, such as a color or an optional feature. Print the Object that’s returned to make sure all the information was stored correctly.
+function storeCarInfo(manufacturer, modelName, ...extraoption) {
+    const carinfo = Object.assign({ manufacturer,
+        modelName }, Object.assign({}, ...extraoption));
+    return carinfo;
+}
+;
+let answer = storeCarInfo('Bugatti', 'Bugatti Divo', { color: 'black' }, { features: ['exhaust pipes', 'NACA duct ON ROOF'] });
+console.log(answer);
