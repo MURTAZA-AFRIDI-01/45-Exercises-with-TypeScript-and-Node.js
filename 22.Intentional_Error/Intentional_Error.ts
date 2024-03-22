941e867df14be2f67e2d577950da8c5b3f936d49
MURTAZA_AFRIDI_99 <160273309+MURTAZA-AFRIDI-01@users.noreply.github.com>
2024-03-23 03:29:22 +0500

    Add files via upload

---
+++ 22.Intentional_Error/Intentional_Error.ts
@@ -0,0 +1,11 @@
+// If you haven’t received an array index error in one of your programs yet, try to make 
+// one happen.
+
+//Creating Array
+let newArray :string[]=['Bugatti Divo','Bugatti Mistral','Bolide']; 
+
+// Asscessing Array  by index
+console.log(newArray[5]);
+// Make sure you correct the error before closing the program.
+// correct the error
+console.log(newArray);
